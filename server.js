ee9c9006d48d9dcb7ca605ea628caadeb59ecb78
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-10 17:17:31 -0500

    Added express.js http server.

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Negative Impact Server Go!'))
+
+app.listen(port, () => console.log(`Negative Impact server listening on ${port}!`))
