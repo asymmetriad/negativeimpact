31de2acdfb009d941b00739f71004798e204dffa
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-12 13:40:58 -0500

    Add skeleton Mongoose/MongoDB setup.

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Negative Impact Server Go!'))
+app.get('/', (req, res) => res.send('Negative Impact Server Go!'));
 
-app.listen(port, () => console.log(`Negative Impact server listening on http://localhost:${port}!`))
+app.listen(port, () => console.log(`Negative Impact server listening on http://localhost:${port}!`));
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Connected to MongoDB test database.");
+});
+
+let mongoTestSchema = new mongoose.Schema({
+    name: String
+});
+
+let mongotest = mongoose.model("mongotest", mongoTestSchema);
+let mangos = new mongotest({ name: 'mangos' });
+
+console.log(mangos.name);
+
+mangos.save(function (err, mangos) {
+    if (err) return console.error(err);
+});
