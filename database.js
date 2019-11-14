68373b5c4c5caa165de56a53e4a7dc65589e4ef4
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2019-11-14 12:39:36 -0500

    Add database.js

---
+++ database.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+//create connection to database
+mongoose.connect('mongodb://localhost/negativeimpact', {useNewUrlParser: true, useUnifiedTopology: true});
+let db = mongoose.connection;
+
+//conection fail
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//conection sucessuces
+db.once('open', function() {
+    console.log("Connected to MongoDB Negative Impact database.");
+});
+
+//
+let userInfoSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+let userInfo = mongoose.model("mongotest", userInfoSchema);
+let user = new userInfo({ username: 'test' ,
+                             password: 'password'});
+
+console.log(user.username + "\n" + user.password);
+
+user.save(function (err, user) {
+    if (err) return console.error(err);
+});
