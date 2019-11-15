581565d0d0771531dc0233e8792fa1532ec34c20
kuqiaaa123 <32714488+kuqiaaa123@users.noreply.github.com>
2019-11-14 20:51:09 -0500

    add bodyParser.json, and express.static

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const mustache = require('mustache-express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-let User = require('./model/user');
-
+app.use("/public", express.static('./public/'));
+app.use(bodyParser.json());
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
-
-app.get('/', (req, res) => res.render('index.mustache', {hello: 'Welcome to Negative Impact!'}));
-
-
 app.listen(port, () => console.log(`Negative Impact server listening on http://localhost:${port}!`));
 
+//Router
+app.use('/',require('./routes/index'));
+app.use('/user',require('./routes/user'));
 
-app.get('/user', function(req,res){
+//mongodb model
+let User = require('./model/user');
+
+app.get('/userdata', function(req,res){
   let user=new User({
     username:"username",
     password:"password"
