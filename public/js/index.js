7eef95b6639077fc641148f0f749df3f222a86f8
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-16 20:02:20 -0500

    Add JSON travel obj, points calc by travel type.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,33 @@
 function carbon_calc() {
     // Magic calculations on clicking the calculate button.
     $("#calculate").click(() => {
-        let gas_pollution = $("#distance").val();
+        let trvtype = $("#traveltype").find(":selected").val();
+        let pts = 0;
+        let dist = $("#distance").val();
+        if(trvtype == "Walk") {
+            pts = (15 * dist) / 3;
+        }
+        else if(trvtype == "Bicycle") {
+            pts = (12 * dist) / 4;
+        }
+        else if(trvtype == "Train") {
+            pts = (8 * dist) / 5;
+        }
+        else if(trvtype == "Bus") {
+            pts = (3 * dist) / 6;
+        }
+        else if(trvtype == "Car") {
+            pts = (-5 * dist) / 4;
+        }
+        else if(trvtype == "Plane") {
+            pts = (-10 * dist) / 3;
+        }
+        alert(pts);
+        let user_travel = {
+            distance: dist,
+            travel_type: trvtype,
+            points: pts
+        };
     });
 }
 
