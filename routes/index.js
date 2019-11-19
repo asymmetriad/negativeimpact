137a5690ad54617a41e02802d4d9664007340e89
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-19 09:47:46 -0500

    Use existing view file over nonexistent one.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => res.render('welcome.mustache',{hello: 'Welcome to Negative Impact!'}));
+router.get('/',(req,res) => res.render('index.mustache',{hello: 'Welcome to Negative Impact!'}));
 
 module.exports = router;
