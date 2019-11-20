fb759dbb43c7508ceb188fe72ada0b1f1a46d56e
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-20 09:34:21 -0500

    Remove database code from server.js.

--- server.js
+++ server.js
@@ -36,23 +36,3 @@ app.post('/form', (req,res,next) => {
 
 
 app.listen(port, () => console.log(`Negative Impact server listening on http://localhost:${port}!`));
-
-mongoose.connect('mongodb://localhost/negativeimpact', {useNewUrlParser: true, useUnifiedTopology: true});
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("Connected to MongoDB Negative Impact database.");
-});
-
-let mongoTestSchema = new mongoose.Schema({
-    name: String
-});
-
-let mongotest = mongoose.model("mongotest", mongoTestSchema);
-let mangos = new mongotest({ name: 'mangos' });
-
-console.log(mangos.name);
-
-mangos.save(function (err, mangos) {
-    if (err) return console.error(err);
-});
