ce40d85bebfd9618d33e626018dfabda78bb1cb2
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-21 09:17:52 -0500

    Remove testing/redundant user routing code in server.js.

--- server.js
+++ server.js
@@ -18,15 +18,3 @@ app.use('/user',require('./routes/user'));
 
 //mongodb model
 let User = require('./model/user');
-
-app.get('/userdata', function(req,res){
-  let user=new User({
-    username:"username",
-      password:"password",
-      distance_traveled: 3000,
-      travel_type: "Car"
-  });
-  user.save(function(err,user){
-    res.send("Username: " + user.username + "\nPassword: " + user.password + user.distance_traveled + user.travel_type);
-  });
-});
