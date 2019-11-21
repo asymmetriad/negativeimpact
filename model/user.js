a2013901539898f1041a2bcb1f7cf3147f8bf661
jiachao chen <32714488+kuqiaaa123@users.noreply.github.com>
2019-11-21 13:21:41 -0500

    Update user.js

--- model/user.js
+++ model/user.js
@@ -6,6 +6,11 @@ const userSchema = new Schema({
     username: {type:String},
     password: {type:String},
     email: {type:String},
+    history: {},
+    rate: {type:Number}, // pollution rate
+    totalMiles: {type:Number}, // total miles user travels
+    numTravel: {type:Number}, // number of tiem user travels
+    state: {type:String}
 });
 
 module.exports = db.model('User',userSchema);
