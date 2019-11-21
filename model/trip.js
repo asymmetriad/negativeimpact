e65c155fc8b624b2e032933c4ff136e112a53e38
Jiachao Chen <jiachaochen@umass.edu>
2019-11-21 13:11:45 -0500

    add trip model

--- model/trip.js
+++ model/trip.js
@@ -3,7 +3,8 @@ const db = require("../db");
 const Schema = db.Schema;
 const tripSchema = new Schema({
     username: {type:String},
-
+    distance: {type:Number},
+    method: {type:String},
 });
 
 module.exports = db.model('User',tripSchema);
