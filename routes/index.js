3b46e381725c6381b673050025790e0d641beade
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-21 09:06:07 -0500

    Move routing logic into routes/index.js from server.js.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => res.render('index.mustache',{hello: 'Welcome to Negative Impact!'}));
+let travel_options = {
+    options: [
+        {value: 'Walk', text: 'Walk', selected: true},
+        {value: 'Bicycle', text: 'Bicycle'},
+        {value: 'Train', text: 'Train'},
+        {value: 'Bus', text: 'Bus'},
+        {value: 'Car', text: 'Car'},
+
+    ]};
+
+router.get('/',(req,res) => {
+    res.render('index.mustache', travel_options);
+});
+
+router.post('/form', (req,res,next) => {
+    let user_data = {
+        distance: req.body.distance,
+        travel_type: req.body.traveltype
+    };
+});
 
 module.exports = router;
