1b343c97be361676e5a8ada5c2e946ad0c075a26
asymmetriad <57491660+asymmetriad@users.noreply.github.com>
2019-11-22 14:56:36 -0500

    Add Haversine formula for plane/low orbit spaceship trips, checkbox change.

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,58 @@ function enter_key(keydown) {
         $('#dest').show();
     }
 }
+function direct_trip_chk(){
+    $('#direct_trip[type=checkbox]').click(() => {
+
+    if ($('#direct_trip[type=checkbox]').prop('checked')) {
+        $('label[for=dest]').hide();
+        $('#dest').hide();
+        $('label[for=start]').hide();
+        $('#start').hide();
+
+    }
+    else {
+        $('label[for=start]').show();
+        $('#start').show();
+    }
+    });
+}
+
+function to_radians(degrees) {
+    return (degrees * (Math.PI/180));
+}
+
 function travel_submit() {
     $('#travel').submit(() => {
+        // Prevent default page behavior, like reloading the page.
         event.preventDefault();
+        if ($('#direct_trip[type=checkbox]').prop('checked')) {
+            $('label[for=dest]').hide();
+            $('#dest').hide();
+            $('label[for=start]').hide();
+        $('#start').hide();
+        // Haversine formula.
+        // The earth's mean radius in miles.
+        let earth_radius = 3958.7613;
+        // User input.
+        let start_lat = 81.4;
+        let start_long = 43.4;
+        let end_lat = 67.4;
+            let end_long = 23.4;
+
+        let sinlat = to_radians(end_lat - start_lat);
+        let sinlong = to_radians(end_long - start_long);
+        let coslat1 = to_radians(start_lat);
+        let coslat2 = to_radians(end_lat);
+        // The square of half the chord length between the two points.
+        let sqr_half_chord = Math.sin(sinlat / 2) * Math.sin(sinlat / 2) +
+            Math.cos(coslat1) * Math.cos(coslat2) * Math.sin(sinlong / 2) * Math.sin(sinlong / 2);
+            let angular_dist = 2 *
+                Math.atan2(Math.sqrt(sqr_half_chord),
+                           Math.sqrt(1-sqr_half_chord));
+        let distance = earth_radius * angular_dist;
+            alert(distance);
+        }
 
         $.post('/form',
                function(res, req) {
@@ -15,5 +64,6 @@ function travel_submit() {
 }
 
 $(() => {
+    direct_trip_chk();
     travel_submit();
 });
