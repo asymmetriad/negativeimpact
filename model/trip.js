e87a04da89b21165a2c36c397922add884fdce8d
Nathan Duarte <nduartech101@gmail.com>
2019-11-27 02:06:42 -0500

    Preliminary work

--- model/trip.js
+++ model/trip.js
@@ -6,4 +6,29 @@ const tripSchema = new Schema({
 
 });
 
+// returns here session object for further use
+function new_here_session() {
+
+}
+
+function get_loc_geo() {
+
+}
+
+function get_car_route() {
+
+}
+
+function get_pub_transport_route() {
+
+}
+
+function get_pedestrian_route() {
+
+}
+
+function get_all_routes() {
+
+}
+
 module.exports = db.model('User',tripSchema);
